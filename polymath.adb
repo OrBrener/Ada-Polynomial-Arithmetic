d82dc8826c064f13d02caf421134cc65765e8850
Or Brener <obrener@uoguelph.ca>
2023-03-02 21:53:38 -0500

    refactor addpoly

--- polymath.adb
+++ polymath.adb
@@ -4,30 +4,38 @@
 
 package body polymath is 
 
-  procedure addpoly(polyA : in termPtr; polyB : in termPtr; polyResult : out termPtr) is
+  function addpoly(polyA : in termPtr; polyB : in termPtr) return termPtr is
     first : termPtr := polyA;
     second : termPtr := polyB;
+    polyResult : termPtr := null;
 
     begin
+
+    -- while both of the polynomials are not done: 
     while (first /= null or second /= null) loop
 
+      -- if the second polynomial is larger than the first (-2x+9 vs. 3x^2+2x-5)
       if (second.degree > first.degree) then
+        -- the sum term is the second polynomial term
         appendTerm(second.coefficient, second.degree, polyResult);
-
         second := second.nextTerm;
 
+      -- if the first polynomial is larger than the second (3x^2+2x-5 vs. -2x+9)
       elsif (first.degree > second.degree) then
+        -- the sum term is the first polynomial term
         appendTerm(first.coefficient, first.degree, polyResult);
-
         first := first.nextTerm;
 
+      -- if both polynomails are the same length
       else
+        -- the sum term is the sum of the two coefficients
         appendTerm(first.coefficient + second.coefficient, first.degree, polyResult);
-
         first := first.nextTerm;
         second := second.nextTerm;
       end if;    
     end loop;
+
+    return polyResult;
 
   end addpoly;
 
@@ -45,7 +53,7 @@ package body polymath is
     second := polyB;
 
     -- A-B = A+(-B) 
-    addpoly(polyA,second,polyResult);
+    polyResult := addpoly(polyA,second);
 
     -- reset the second polynomial to it's original state
     while (second /= null) loop
--- polymath.ads
+++ polymath.ads
@@ -5,7 +5,7 @@ with polynomial; use polynomial;
 
 package polymath is
     
-    procedure addpoly(polyA : in termPtr; polyB : in termPtr; polyResult : out termPtr);
+    function addpoly(polyA : in termPtr; polyB : in termPtr) return termPtr;
     function subpoly(polyA : in termPtr; polyB : in termPtr) return termPtr;
     --  procedure multpoly(polyA : in termPtr; polyB : in termPtr; polyResult : out termPtr);
     --  procedure evalpoly(polyA : in termPtr; evalNum : in integer; polyResult : out termPtr);
