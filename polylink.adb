b4acb5e54a1e5617713473a46722690278f4ba05
Or Brener <obrener@uoguelph.ca>
2023-03-02 21:20:17 -0500

    refactored writePOLY

--- polylink.adb
+++ polylink.adb
@@ -30,14 +30,29 @@ package body polylink is
         numTerms : integer := 0;
       begin
 
+      --  for all the terms in the polynomial
       while currentTerm /= null loop
-        numTerms := numTerms + 1;
-        if (currentTerm.coefficient /= 0) then
 
+        -- if the coefficient is zero, skip to the next term
+        if (currentTerm.coefficient = 0) then
+          currentTerm := currentTerm.nextTerm;
+        -- otherwise (coefficent != 0): 
+        else
+          -- increment number of terms & re-initilize isPositive
+          numTerms := numTerms + 1;
+          isPositive := true;
+
+          -- check if the coefficent is positive or negative
           if (currentTerm.coefficient < 0) then
             isPositive := false;
           end if;
+          
+          -- add a negaive sign for the leading term if the coefficient is negative 
+          if ((numTerms = 1) and (isPositive = false)) then
+            put("- ");
+          end if;
 
+          -- put a "+" / "-" on all terms after the leading term
           if (numTerms /= 1) then 
             if (isPositive) then
               put("+ ");
@@ -46,20 +61,27 @@ package body polylink is
             end if;
           end if;
 
-          put(integer'image(abs(currentTerm.coefficient)));
-          
-          if (currentTerm.degree > 0) then
-            put(" x ^");
-            put(integer'image(currentTerm.degree));
-            put("  ");
-          elsif (currentTerm.degree = 0) then
-            put_line("");
+          -- print the absolute value of the coefficient
+          -- do not print the coefficient if it is 1 (ex: 1x^2 = x^2), UNLESSS it is the last term (5x + 1)
+          if ((abs(currentTerm.coefficient) > 1) or (currentTerm.degree = 0)) then
+            put(integer'image(abs(currentTerm.coefficient)));
           end if;
-        end if;
 
-        currentTerm := currentTerm.nextTerm;
+          -- print the degree of x
+          if (currentTerm.degree > 1) then
+            put(" x ^" & integer'image(currentTerm.degree) & "  ");
+          elsif (currentTerm.degree = 1) then
+            put(" x  ");
+          end if;
+
+          -- continue to the next term in the polynomial
+          currentTerm := currentTerm.nextTerm;
+          
+        end if;
       end loop;
+      -- print an ending newline
+      put_line("");
     end writePOLY;
     
-
+  
 end polylink;
